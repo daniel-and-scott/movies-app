e70d84e83ee9b60ed0eecbc785a2f6f9beb5ab5e
Scott Carpenter <carpenter.scott22@gmail.com>
2022-02-22 09:24:32 -0600

    removed navbar home

--- js/button-functionality.js
+++ js/button-functionality.js
@@ -1,1 +1,13 @@
+"use strict";
+
+//TODO add href to home button
+
+//TODO add movies button functionality
+
+//TODO add DELETE button functionality
+
+// TODO add CHANGE button functionality
+
+//TODO search bar functionality
+
 
