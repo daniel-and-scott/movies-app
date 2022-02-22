b2e74a4867b638431de24e19f14ec46596197a2d
daniel ficci <danielficci@gmail.com>
2022-02-22 11:24:07 -0600

    working on button functionality

---
+++ js/display-info.js
@@ -0,0 +1,35 @@
+const showMovieInfo = $('#movie-cards')
+
+function showMovies(data) {
+    let allMovies = '';
+
+    data.forEach((id) => {
+        //language=html
+        allMovies = `
+            <div class="card" style="width: 18rem;">
+                <div class="card-header bg-dark">
+                    <h5 class="card-title text-center movie-title text-white">${id.title}</h5>
+                </div>
+                <div class="card-body">
+                    <div class="poster card-text mb-3">Poster: ${id.poster}</div>
+                    <p class="rating card-text">Rating: ${id.rating}</p>
+                    <p class="year card-text">Year: ${id.year}</p>
+                    <p class="genre card-text">Genre: ${id.genre}</p>
+                    <p class="director card-text">Director: ${id.genre}</p>
+                    <p class="plot card-text">Plot: ${id.plot}</p>
+                    <p class="actors card-text">Actors: ${id.actors}</p>
+                </div>
+                <div class="card-footer">
+                    <a href="#" data-id="${id.id}" class="btn btn-outline-success float-right edit-information-btn"
+                       data-toggle="modal"
+                       data-target="#editInfo">Edit This movie</a>
+                </div>
+            </div>`
+        showMovieInfo.append(allMovies)
+    });
+}
+
+$(document).on('click', '.edit-information-btn', function () {
+    $('.edit-submit-button').attr( this, 'data-id')
+
+})
--- js/movie-script.js
+++ js/movie-script.js
@@ -2,20 +2,24 @@
 function getAllMovies() {
     fetch(dataBaseUrl)
         .then(res => res.json()
-            .then(movies => console.log(movies)))
+            .then(movies => showMovies(movies)))
 }
 
 getAllMovies()
 
 //TODO: Create function that takes the title and displays it
+
 function getTheTitle() {
     fetch(dataBaseUrl)
         .then(res => res.json()
-            .then(title => console.log(title[1].title)))
+            .then(title => console.log(title[0].title)))
 }
 
+getTheTitle()
 
 //TODO: Add movie to list
+
+
 function addMoviesToList(newMoviesToAdd) {
 
     const options = {
@@ -32,6 +36,7 @@ function addMoviesToList(newMoviesToAdd) {
 }
 
 //TODO: Create function that will delete previous inputs
+
 function deleteMoviesFromList(id) {
 
     const deleteRequest = {
@@ -47,29 +52,22 @@ function deleteMoviesFromList(id) {
 }
 
 //TODO: Create function that will change information within the API
-
-function changeInformation(id) {
-    const change = {
-        title: $('#title-change').val(),
-        rating: $('#rating-change').val(),
-        poster: $('#poster-change').val(),
-        year: $('#year-change').val(),
-        genre: $('.list-of-genres').val(),
-        director: $('#director-change').val(),
-        plot: $('#plot-change').val(),
-        actors: $('#actors-change').val(),
-    }
-    const putRequest = {
-        method: "PUT",
-        headers: {
-            "Content-type": "application/json",
-        },
-        body: JSON.stringify(change)
-    }
-
-    fetch(`${dataBaseUrl}/${id}`, putRequest)
-        .then(res => res.json()
-            .then(info => console.log(info)))
-}
-
-
+// var info = {
+//     title: 'ASDF'
+// }
+//
+// function changeInformation(change ,id) {
+//
+//     const putRequest = {
+//         method: "PUT",
+//         headers: {
+//             "Content-type": "application/json",
+//         }
+//     }
+//     fetch(`${dataBaseUrl}/${id}`, putRequest)
+//         .then(res => res.json()
+//         .then(info => console.log(info)))
+//
+// }
+//
+// changeInformation(info,2)
